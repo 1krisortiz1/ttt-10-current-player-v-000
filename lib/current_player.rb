c272d5aed06a3b0f6fd5f3ce8fb6e717b53b787c
Kristin Ortiz <itskristin.ortiz@gmail.com>
2017-12-15 20:01:13 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,9 @@
 def turn_count(board)
-counter = 0
-  board.each do |index|
-  if index == "X" || index == "O"
+  counter = 0
+    board.each do |index|
+      if index == "X" || index == "O"
   counter += 1
-end
-end
-counter
+      end
+    end
+  counter
 end
