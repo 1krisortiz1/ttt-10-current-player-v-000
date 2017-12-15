98c8760f1592981476033ecc754e202586b9d023
Kristin Ortiz <itskristin.ortiz@gmail.com>
2017-12-15 19:40:50 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,17 +1,12 @@
 index = ["X","O","X","O","X","O","X","O","X"]
-players_turn=index.count
 counter = 0
-
-loop do
-  if counter<players_turn
-  puts "it's your turn #{index[counter]}"
-
-#def turn_count(index)
+def turn_count(board)
+  board.each do |next|
+  if next == "X" || next == "O"
   counter += 1
-  break
-#end
 end
 end
+
 def current_player(board)
   if turn_count(board % 2 == 0)
     put "X"
