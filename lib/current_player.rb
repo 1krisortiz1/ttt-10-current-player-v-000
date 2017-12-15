4fc487d843724a13ccce3ceb6473f0aaf6010750
Kristin Ortiz <itskristin.ortiz@gmail.com>
2017-12-15 18:56:39 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,14 +1,12 @@
 def turn_count(board)
   counter = 0
 board.each do |index|
-  puts "It's your #{turn}"
-
-   if 4 % 2
-      counter =+ 1
+  if index !=" "
+  counter += 1
    end
 
 end
 
 #def current_player
 
-3end
+#end
