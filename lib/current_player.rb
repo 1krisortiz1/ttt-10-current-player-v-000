2b3a36ba53721e14045b5885ebfc7c982f2c7612
Kristin Ortiz <itskristin.ortiz@gmail.com>
2017-12-15 19:45:19 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,7 @@
 index = ["X","O","X","O","X","O","X","O","X"]
 counter = 0
 def turn_count(board)
-  board.each do |index
-    |
+  board.each do |index|
   if index == "X" || index == "O"
   counter += 1
 end
