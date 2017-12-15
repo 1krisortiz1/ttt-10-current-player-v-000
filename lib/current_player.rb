3a79484a9b8d2f5eb6d142fdb54d9fb612658a7b
Kristin Ortiz <itskristin.ortiz@gmail.com>
2017-12-15 18:42:52 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,14 @@
+player = ["turn X","turn O","turn X","turn O","turn X","turn O","turn X","turn O"."turn X"]
+def turn_count
+player.each do |turn|
+  puts "It's your #{turn}"
+counter = 0
+   if condition
+      counter =+ 1
+   end
+  
+end
+
+def current_player
+  
+end
