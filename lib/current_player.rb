039cc5665908f1e8a9c10a3cf416f1ed59b27bf3
Kristin Ortiz <itskristin.ortiz@gmail.com>
2017-12-15 18:46:31 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,9 @@
 player = ["turn X","turn O","turn X","turn O","turn X","turn O","turn X","turn O"."turn X"]
-def turn_count
+def turn_count(turn)
 player.each do |turn|
   puts "It's your #{turn}"
 counter = 0
-   if condition
+   if 4 % 2
       counter =+ 1
    end
 
